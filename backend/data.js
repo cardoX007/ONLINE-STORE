4d77a60b9e0a125196c6211da61ebb279377254e
Richard Pepple <cardoelcapone@gmail.com>
2020-08-05 04:35:21 -0700

    Data fetched from server By Richard Pepple

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default{
+ products:[ 
+        {
+        _id: "1",
+        name: 'Slim Shirt',
+        category:'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id: "2",
+        name: 'Denim Pant',
+        category:'Pants',
+        image: '/images/d1.jpg',
+        price: 40,
+        brand: 'Zara',
+        rating: 2.8,
+    numReviews: 10
+    },
+    {
+        _id: "3",
+        name: 'Running Shoes',
+        category:'Shoes',
+        image: '/images/d2.jpg',
+        price: 70,
+        brand: 'Addidas',
+        rating: 3.5,
+        numReviews: 10
+    }
+    ]
+}
